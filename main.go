f5cd783539a4506cbff84c5f55dbc8dc35d53b59
Benjamin Toll <benjam72@yahoo.com>
2021-12-25 01:47:28 -0500

    Added concurrency and channels

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"net/url"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type Link struct {
+	Owner      string
 	URL        string
 	StatusCode int
 }
@@ -25,67 +27,36 @@ type ScannerSession struct {
 	TotalFiles int
 	Tree       map[string][]Link
 	Links      []Link
+	Failed     []Link
 }
 
-/*
-func getHead(c chan Link, link string) {
-	_, err := url.ParseRequestURI(link)
+func getHead(c chan Link, l Link) {
+	_, err := url.ParseRequestURI(l.URL)
 	if err != nil {
-		c <- Link{
-			URL:        link,
-			StatusCode: -1,
-		}
+		l.StatusCode = -1
+		c <- l
 		return
 	}
 
-	resp, err := http.Head(link)
+	resp, err := http.Head(l.URL)
 	if err != nil {
-		c <- Link{
-			URL:        link,
-			StatusCode: 401,
-		}
+		l.StatusCode = 401
+		c <- l
 		return
 	}
 
-	c <- Link{
-		URL:        link,
-		StatusCode: resp.StatusCode,
-	}
-}
-*/
-
-func getHead(link string) Link {
-	_, err := url.ParseRequestURI(link)
-	if err != nil {
-		return Link{
-			URL:        link,
-			StatusCode: -1,
-		}
-	}
-
-	resp, err := http.Head(link)
-	if err != nil {
-		return Link{
-			URL:        link,
-			StatusCode: 401,
-		}
-	}
-
-	return Link{
-		URL:        link,
-		StatusCode: resp.StatusCode,
-	}
+	l.StatusCode = resp.StatusCode
+	c <- l
 }
 
 func (ls *LinkScanner) getLinkScannerSession() (ScannerSession, error) {
 	var files []string
 	var links []Link
+	var failed []Link
 	var err error
 	var numFiles int
 
-	urls := make(map[string][]Link, 1000)
-
-	//	c := make(chan Link, len(urls))
+	urls := make(map[string][]string, 1000)
 
 	if ls.Dir != "" {
 		files, err = ls.readDirByFileType()
@@ -94,46 +65,47 @@ func (ls *LinkScanner) getLinkScannerSession() (ScannerSession, error) {
 		}
 
 		for _, filename := range files {
-			files, err := ls.readFileByPattern(filename)
+			fs, err := ls.readFileByPattern(filename)
 			if err != nil {
 				fmt.Println(err)
 			}
 
-			//			urls[filename] = fs
-			for _, file := range files {
-				links = append(links, getHead(file))
+			urls[filename] = fs
+
+			for _, f := range fs {
+				files = append(files, f)
 			}
 
-			urls[filename] = links
 			numFiles += 1
 		}
 	}
 
-	/*
-		for _, v := range urls {
-			for _, url := range v {
-				go getHead(c, url)
+	c := make(chan Link, len(urls))
+
+	for filename, v := range urls {
+		for _, url := range v {
+			l := Link{
+				Owner: filename,
+				URL:   url,
+			}
+			go getHead(c, l)
+		}
+	}
+
+	for _, v := range urls {
+		for i := 0; i < len(v); i++ {
+			link := <-c
+			links = append(links, link)
+			if link.StatusCode == 401 {
+				failed = append(failed, link)
 			}
 		}
-	*/
-
-	/*
-		for _, filename := range files {
-
-			//		fmt.Println("filename", filename)
-			//			links = append(links, <-c)
-			for i := 0; i < len(v); i++ {
-				links = append(links, <-c)
-			}
-
-			urls[key] = links
-		}
-	*/
+	}
 
 	return ScannerSession{
 		TotalFiles: numFiles,
-		Tree:       urls,
 		Links:      links,
+		Failed:     failed,
 	}, err
 }
 
@@ -190,8 +162,9 @@ func main() {
 	dir := flag.String("dir", "", "If given, searches every file for a match.")
 	filename := flag.String("filename", "", "If given a directory, will only search by this filetype.  Defaults to `md`.")
 	filetype := flag.String("filetype", ".md", "If given a directory, will only search by this filetype.  Defaults to `md`.")
-
 	flag.Parse()
+
+	urlIgnore := `\.onion|example\.com|<YOUR_DOMAIN>`
 
 	ls := LinkScanner{
 		Dir:      *dir,
@@ -205,14 +178,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	for filename, links := range ss.Tree {
-		fmt.Printf("\n%s\n", filename)
-
-		for _, link := range links {
-			fmt.Printf("\t%s\n", link)
+	if len(ss.Failed) > 0 {
+		split := strings.Split(urlIgnore, "|")
+		fmt.Printf("[FAILED] Number of failures = %d\n", len(ss.Failed))
+		fmt.Println("Ignored all URLs that contained any of the following:")
+		fmt.Println(strings.Join(split, "\n"))
+		re := regexp.MustCompile(urlIgnore)
+		for _, link := range ss.Failed {
+			if re.MatchString(link.URL) {
+				continue
+			}
+			if link.StatusCode == 401 {
+				fmt.Printf("\n(%d) %s\tOwner: %s", link.StatusCode, link.URL, link.Owner)
+			}
 		}
-		//		if link.StatusCode == 401 {
-		//			fmt.Println(link)
-		//		}
+		// Add newline so it's not on the same line as the cursor.
+		fmt.Println()
+		os.Exit(1)
+	} else {
+		fmt.Println("No failures.")
 	}
 }
