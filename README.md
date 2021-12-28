0b3f3f8678c24c66ca9af9188049134aa6423a96
Benjamin Toll <benjam72@yahoo.com>
2021-12-28 13:55:55 -0500

    Added `quiet` and `verbose` flags

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # LinkScanner
 
 ```
-Usage of ./link-scanner:
+Usage of link-scanner:
   -dir string
-        Optional.  Searches every file in the directory for a match.  Non-recursive. (default ".")
+        Optional.  Searches every file in the directory for a match.  Non-recursive.
   -filename string
         Optional.  Takes precedence over directory searches.
-  -filetype string
-        Only searches files of this type. (default ".md")
+  -filetype .html
+        Only searches files of this type.  Include the period, i.e., .html (default ".md")
+  -q    Optional.  Turns on quiet mode.
+  -v    Optional.  Turns on verbose mode.
 ```
 
 ## Git `pre-commit` Hook
@@ -33,14 +35,4 @@ Finally, add the hook as a local config:
 ```
 git config --local --add hooks.pre-commit.hook link-scanner.sh
 ```
-
-## TODO
-
-- Add verbose flag.
-    + Will show what files are ignored.
-- Add quiet flag.
-    + Will suppress the `FAILED` line with number of failures.
-    + Will enable easier `stdout` parsing by subsequent programs in a pipeline.
-- Defaults to only showing failures.  This is determined to be `401`s.  Let user decide.
-- Add color?
 
--- main.go
+++ main.go
@@ -35,6 +35,8 @@ func main() {
 	dir := flag.String("dir", "", "Optional.  Searches every file in the directory for a match.  Non-recursive.")
 	filename := flag.String("filename", "", "Optional.  Takes precedence over directory searches.")
 	filetype := flag.String("filetype", ".md", "Only searches files of this type.  Include the period, i.e., `.html`")
+	verbose := flag.Bool("v", false, "Optional.  Turns on verbose mode.")
+	quiet := flag.Bool("q", false, "Optional.  Turns on quiet mode.")
 	flag.Parse()
 
 	ls := LinkScanner{
@@ -50,29 +52,42 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("[INFO] Number of files scanned = %d\n", ss.TotalFiles)
-	fmt.Printf("[INFO] Number of links matched = %d\n", len(ss.Links))
-	fmt.Printf("[INFO] Links failed  = %d\n", len(ss.Failed))
-	fmt.Printf("[INFO] Links skipped = %d\n", len(ss.Skipped))
-	fmt.Printf("[INFO] Skip pattern list:\n%s\n", strings.Join(strings.Split(ls.SkipPattern, "|"), "\n"))
+	if !*quiet {
+		fmt.Printf("[INFO] Number of files scanned = %d\n", ss.TotalFiles)
+		fmt.Printf("[INFO] Number of links matched = %d\n", len(ss.Links))
+		fmt.Printf("[INFO] Links skipped = %d\n", len(ss.Skipped))
+		fmt.Printf("[INFO] Links failed  = %d\n", len(ss.Failed))
+	}
 
-	/*
+	if *verbose {
+		fmt.Println("\n[DEBUG] -----------------------------------------------------------------------------")
+		l := strings.Split(ls.SkipPattern, "|")
+		s := strings.Join(l, ",")
+		fmt.Printf("[DEBUG] Skip pattern list: %s\n", strings.ReplaceAll(s, "\\", ""))
+		fmt.Println("[DEBUG] The following files were skipped because of a match in the skip pattern list:")
+		fmt.Println("[DEBUG] -----------------------------------------------------------------------------")
 		if len(ss.Skipped) > 0 {
 			for _, link := range ss.Skipped {
-				fmt.Printf("\n(%d)   (Link)  %s\n\t(Error) %s\n\t(Owner) %s\n", link.StatusCode, link.URL, link.Error, link.Owner)
+				fmt.Printf("[DEBUG] - %s\n", link.URL)
 			}
 		}
-	*/
+		fmt.Println("[DEBUG] -----------------------------------------------------------------------------")
+		fmt.Println()
+	}
 
 	if len(ss.Failed) > 0 {
-		for _, link := range ss.Failed {
-			fmt.Printf("\n(%d)   (Link)  %s\n\t(Error) %s\n\t(Owner) %s\n", link.StatusCode, link.URL, link.Error, link.Owner)
+		if !*quiet {
+			for _, link := range ss.Failed {
+				fmt.Printf("\n(%d)   (Link)  %s\n\t(Error) %s\n\t(Owner) %s\n", link.StatusCode, link.URL, link.Error, link.Owner)
+			}
 		}
 
 		if len(ss.Skipped) != len(ss.Failed) {
 			os.Exit(1)
 		}
 	} else {
-		fmt.Println("[SUCCESS] No failures.")
+		if !*quiet {
+			fmt.Println("[SUCCESS] No failures.")
+		}
 	}
 }
